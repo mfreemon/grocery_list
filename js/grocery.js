1d7e2ebd68521dfbc231a6a2db0f5b88760b7a35
Marcus Freemon <dfreemon@gmail.com>
2015-04-17 17:19:32 -0500

    bug fixes

--- js/grocery.js
+++ js/grocery.js
@@ -1,28 +1,49 @@
+// var comments = '<ul id ="comment-list"><li><div class="comments large-10 columns"><p>See if this works</p></div><li></ul>';
+// var groceries = '<h3>'+'<div class="item large-10 columns">'+'<p>'+'dog'+'</p>' 
+// 					+'</div>'+'</h3>'+'<div class="comments large-10 columns">'
+// 					+'<p>'+'See if this works'+'</p>'+'</div>';
+
+
+
+
 
 
 	function Accordion(){	
-		$("#comments-tab").click(function(){
-		$('.comments').slideToggle();
+		$(".grocery-cell").on('click','#comments-tab', function(event){
+			event.preventDefault();
+			$('.comment-list').slideToggle();
 	})
 };
 
-	function Append (){
-	$('button').on('click', function(event){
-		event.preventDefault();
-		var grocery_item = $('input').val();
-		console.log(grocery_item);	
-		$('.shopping-list').append('<h3><div class="grocery-cell large-10 columns"><p>'+grocery_item+'</p></div></h3><div class="comments large-10 columns"><p>See if this works</p></div></h3>');		
-		$("#accordion").accordion('refresh');
-		console.log('we made it');
-		$('input').val('');				
-		})	
-	};
 
-	var groceries = '<h3>'+'<div class="item large-10 columns">'+'<p>'+'dog'+'</p>' 
-					+'</div>'+'</h3>'+'<div class="comments large-10 columns">'
-					+'<p>'+'See if this works'+'</p>'+'</div>';
+	function Checkoff(){
+		$('#checkbox1').on('change', function(){ 
+			if ($(this).is(':checked')) {
+    			 $('#grocery-item').css('text-decoration', 'line-through');
+ 			 } else {
+     		$('#grocery-item').css('text-decoration', 'none');
+  			}	
+		})
+	};
+ //  			
+	// 	})
+	// };
+
+	// function Append (){
+	// $('button').on('click', function(event, x){
+	// 	event.preventDefault();
+	// 	x = $('input').val();
+	// 	console.log(x);	
+	// 	$('.shopping-list').append('<div class="grocery-cell large-10 columns"><input type ="checkbox" id ="checkbox1"><p id="grocery-item">'+x+'</p><p id="comments-tab"><a href="#" alt ="View Comments">Comments</a></p></div>' + comments);		
+	// 	console.log('we made it');
+	// 	$('input').val('');				
+	// 	})	
+	// };
+
+	
 $(document).ready(function(){
 	Accordion();
-	Append();
+	// Append();
+	Checkoff();
 
 });
