f9d651e5eb72030dccb984d892afbd73414ebaaf
Marcus Freemon <dfreemon@gmail.com>
2015-04-19 22:52:26 -0500

    comments section reorganize

--- js/grocery.js
+++ js/grocery.js
@@ -1,39 +1,34 @@
 
 $(document).ready(function(){
 
-var comments = '<div class = "comment-list large-12 columns"><ul><li class="comments">This is a Comment</li></ul></div>';
+var comments = '<div class = "comment-list large-12 columns"><ul></ul></div>';
+var comment_text ='<div class = "new-comment" ><textarea type ="text" id="textbox"  placeholder="Add a Comment"></textarea></div>'; 
 //var grocery_cell =' <div class="grocery-cell large-12 columns"><div class ="grocery-item large-12 columns"><input type ="checkbox" id ="checkbox1"><p id ="food"></p><p id = "delete"><a href="#"><img src="img/delete.png"></a></p><p id="comments-tab"><a href="#" alt ="View Comments">Comments</a></p></div> + comments +' '</div>';
 
+// var comments = function(comment){
+// 		'<div class = "comment-list large-12 columns"><ul><li class="comments">' + comment +'</li></ul></div>';
+// };	
 
-	$('body').css('background-image','url(./img/wood.jpg)');
+
+	//$('body').css('background-image','url(./img/wood.jpg)');
 	$('button').on('click', append); 	
 	$(".shopping-list").on('click','#comments-tab', accordion); 	
 	$('.shopping-list').on('change', '.grocery-item #checkbox1', checkoff);
 	$('.shopping-list').on('click','.grocery-item #delete', remove);
-	$('.shopping-list').on('click', '.grocery-item #add-tab',new_comment);
-	
-	$('#textbox').keypress(function(event){
- 		text_comm =$('#textbox').val();
-		var keycode = (event.keyCode ? event.keyCode : event.which);
-		if(keycode == '13'){
-			$('.comment-list ul').append('<li class ="comments">'+ text_comm+'</li>');
-			alert('You pressed a "enter" key in textbox');
-			$('#textbox').val('');	
-	}
-	event.stopPropagation();
-});
+	$('.shopping-list').on('click', '.grocery-item #addtab', new_comment);	
+	$('.shopping-list').keypress('#textbox', add_comment);
  	
 		
 
 	function new_comment(event){
 		event.preventDefault();
-		$(this).closest('.grocery-cell').children('.new-comment','.comment-list').slideToggle();
+		$(this).closest('.grocery-cell').children('.new-comment').slideToggle("slow", "swing");
 		//$(this).closest('.grocery-cell').children('.comment-list').slideToggle();
 	}
 
 	function accordion(event){
 		event.preventDefault();
-		$(this).closest('.grocery-cell').children('.comment-list').slideToggle();
+		$(this).closest('.grocery-cell').children('.comment-list').slideToggle("slow", "swing");
 	}
 
 	function remove(event){
@@ -48,24 +43,30 @@ var comments = '<div class = "comment-list large-12 columns"><ul><li class="comm
      		$(this).next('#food').css('text-decoration', 'none');
   			}	
 	}
-
-	// function add_comment(){
-	// 	if (event.keyCode == 13){
-	// 		event.preventDefault();
-	// 		alert('slowly');
-	// 		var text_comm  = $(this).val();
-	// 		$('.comment-list ul').append('<li class ="comments">'+ text_comm+'</li>');
-	// 		$('textarea').val('');				
-	// 	}	
-	// }
-
-
+	
+	function add_comment(){
+		var text_comm = $('#textbox').val();
+		console.log(text_comm);
+		var keycode = (event.keyCode ? event.keyCode : event.which);
+		if(keycode == '13'){
+			$('.comment-list ul').append('<li class="comments">This is a Comment</li>');
+			alert('You pressed a "enter" key in textbox');	
+			$('#textbox').val('');
+		}		
+	}
+	
 	function append (event){
 		event.preventDefault();
 		var x = $('input').val();
-		//console.log(x);	
-		$('.shopping-list').append('<div class="grocery-cell large-12 columns"><div class ="grocery-item large-12 columns"><input type ="checkbox" id ="checkbox1"><p id ="food">' + x + '</p><p id = "delete"><a href="#"><img src="img/delete.png"></a></p><p id="comments-tab"><a href="#" alt ="View Comments">Comments</a></p></div>' + comments + '</div>');		
-		console.log('we made it');
+		var groceries=[
+			'<div class="grocery-cell large-12 columns">',
+			'<div class ="grocery-item large-12 columns">',
+			'<input type ="checkbox" id ="checkbox1">',
+			'<p id="food">'+ x + '</p><p id = "delete"><a href="#"><img src="img/delete.png"></a></p>',
+			'<p id="comments-tab"><a href="#" alt ="View Comments">Comments</a></p>',
+			'<p id="addtab"><a href="#">Add a Comment</a></p></div>'
+		];
+		$('.shopping-list').append(groceries.join('') + comment_text + comments+'</div>');	
 		$('input').val('');					
 	}
 });	
